22ef1a9be986101282ce47b2dfeaf6405e9b59b1
Kuantize Strigoi <adrian.sunye@gmail.com>
2023-01-15 23:20:47 +0100

    created test for listEvents

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -5,11 +5,11 @@
   
 <header class="px-6 py-4 space-y-2 text-center">
   <h1 class="font-serif text-3xl text-sky-600 dark:text-sky-500">More</h1>
-  @auth
+  
     @can('create events')
   <a class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-center text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md dark:text-sky-200 bg-sky-800 hover:bg-sky-700 active:bg-sky-900 focus:outline-none focus:border-sky-900 focus:shadow-outline-sky" href="{{ route('events.create') }}">Create new event</a>
     @endcan
-  @endauth
+  
 </header>
 <main class="grid w-full gap-4 px-4 max-w-7xl sm:grid-cols-2 md:grid-cols-3">
   @foreach($events as $event)
